f65498f45ae4288f19563f89f21b2924714c16c2
bangerlist <bangerlist@gmail.com>
2017-03-29 17:35:02 -0700

    Create app.js

---
+++ firebasetest/app.js
@@ -0,0 +1,12 @@
+(function() {
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCf865M75QLYzGczbBeHbrX6_BEDV8pDic",
+    authDomain: "bangerlist-d8359.firebaseapp.com",
+    databaseURL: "https://bangerlist-d8359.firebaseio.com",
+    storageBucket: "bangerlist-d8359.appspot.com",
+    messagingSenderId: "990833137246"
+  };
+  firebase.initializeApp(config);
+
+}());
