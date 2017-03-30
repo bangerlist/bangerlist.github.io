6b8ae4723ac425276b0df97baa59e724f71e7726
bangerlist <bangerlist@gmail.com>
2017-03-29 19:27:32 -0700

    Update app.js

--- firebasetest/app.js
+++ firebasetest/app.js
@@ -8,5 +8,21 @@
     messagingSenderId: "990833137246"
   };
   firebase.initializeApp(config);
-
+  
+  const txtEmail = document.getElementById('txtEmail');
+  const txtPassword = document.getElementById('txtPassword');
+  const btnLogin = document.getElementById('btnLogin');
+  const btnSignUp = document.getElementById('btnSignUp');
+  const btnLogout = document.getElementById('btnLogout');
+  
+  //login event
+  btnLogin.addEventListener('click', e => {
+    const email = txtEmail.value;
+    const pass = txtPassword.value;
+    const auth = firebase.auth();
+    
+    const promise = auth.signInWithEmailAndPassword(email, pass);
+    
+    promise.catch(e => console.log(e.message));
+  });
 }());
