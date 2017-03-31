071d2a7ab2bb87bfbe84a35dae3c1c729fad5d7a
bangerlist <bangerlist@gmail.com>
2017-03-30 22:56:11 -0700

    Update loginjs.js

--- firebasetest/auth/loginjs.js
+++ firebasetest/auth/loginjs.js
@@ -1,16 +1,4 @@
 
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyCf865M75QLYzGczbBeHbrX6_BEDV8pDic",
-    authDomain: "bangerlist-d8359.firebaseapp.com",
-    databaseURL: "https://bangerlist-d8359.firebaseio.com",
-    storageBucket: "bangerlist-d8359.appspot.com",
-    messagingSenderId: "990833137246"
-  };
-  firebase.initializeApp(config);
-</script>
-  <script type="text/javascript">
-
     /**
      * Handles the sign in button press.
      */
